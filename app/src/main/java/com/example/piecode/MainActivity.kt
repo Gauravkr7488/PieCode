2f8155cbb8985a315857ad9b8d186cfd2ab11f36
Gaurav Kumar <gauravkr748800@gmail.com>
2025-03-18 10:14:48 +0530

    version 0.2 added a navbar(topBar)

--- app/src/main/java/com/example/piecode/MainActivity.kt
+++ app/src/main/java/com/example/piecode/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,9 +22,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PieCodeTheme {
-                CodeEditor() // this is here because you want to run this
+                MainScreen() // this is here because you want to run this
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("PieCode") } // Title for the navigation bar
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+        CodeEditor(Modifier.padding(paddingValues)) // Adds padding so text isn't under the navbar
     }
 }
 
@@ -29,12 +49,13 @@ fun CodeEditor(modifier: Modifier = Modifier) {
     BasicTextField(
         value = text,
         onValueChange = { text = it }, // Updates the text as the user types
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CodeEditorPreview() { // this is a function that shows the preview
-    CodeEditor() // this is the function that is shown in the preview
+    MainScreen()
+   // CodeEditor() // this is the function that is shown in the preview
 }
